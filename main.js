dbba579118a42d427de8af21092e1e250ccf0615
odilovdavlatbek <152914052+odilovdavlatbek@users.noreply.github.com>
2023-12-09 17:57:26 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+// let soni = +prompt("to'g'ri yechgan  savolar sonnini kiriting ")
+// let age = +prompt("har bita savolga necha bal olin ganini yozing ")
+// let bal = soni * age
+// if (80 < bal) {
+//     console.log("siz qabul qilindingiz!");
+
+// }
+// else {
+// console.log("afsuski qabul qilinmadiz")
+// }
+
+
+let soni1 = +prompt("to'g'ri yechgan  savolar sonnini kiriting ")
+let age1 = +prompt("har bita savolga necha bal olin ganini yozing ")
+let bal1 = soni * age
+if (80 < bal) {
+    console.log("siz qabul qilindingiz!");
+    let pul = +prompt("qancha mablag'iz bor ")
+    if (2999 < pul) {
+        console.log("sizni qabul qilamiz!")
+    }
+    else {
+        console.log(" lekin afsuski yetarli mabilag'iz yoq ekan")
+    }
+}
+else {
+    console.log("afsuski qabul qilinmadiz")
+}
